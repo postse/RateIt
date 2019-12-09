7956cd3a9fa515ea33e20cd935dc357bcdf365d3
ryanh777 <ryanh777@ryanh777-pc.wireless-1x.unc.edu>
2019-12-09 15:29:20 -0500

    started accounts; need cleanup

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ $(document).ready(async function () {
         baseURL: "http://localhost:3000/public"
     });
 
+    $("body").on("click", "#signup-btn", function() {
+        let user = $("#username-sign").val();
+        let pass = $("#password-sign").val();
+        let gender = $("#gender").val();
+        let date = $("#birthdate").val();
+        addUser(user, pass, gender, date);
+    });
+
+    $("body").on("click", "#login-btn", function() {
+        let user = $("#username-login").val();
+        let pass = $("#password-login").val();
+        login(user, pass);
+    });
+
+    $("body").on("click", "#test", function() {
+        getStatus();
+    });
+    
     async function getListsToSort() {
         return await publicRoot.get("/listsToSort")
     }
@@ -26,4 +44,50 @@ function populateFromPublicLists(lists) {
         let clickedListName = $(e.target).closest(".listItem").find(".listItemName").html();
         window.location.href = 'selectionScreen.html#' + clickedListName;
     })
+}
+
+async function addUser(user, password, gender, date) {
+    const response = await axios({
+        method: 'POST',
+        url: 'http://localhost:3000/account/create',
+        data: {
+            "name": user,
+            "pass": password,
+            "data": {
+                "gender": gender,
+                "birthdate": date
+            }
+        }
+    });
+    if (response.status == 200) {
+        $("[data-dismiss=modal]").trigger({ type: "click" });
+        //$("#signup-info").text = response.data.status;
+    }
+}
+
+async function login(user, password) {
+    const response = await axios({
+        method: 'POST',
+        url: 'http://localhost:3000/account/login',
+        data: {
+            "name": user,
+            "pass": password,
+        }
+    })
+    console.log(response);
+    if (response.status == 200) {
+        localStorage.setItem("jwt", response.data.jwt);
+        $("[data-dismiss=modal]").trigger({ type: "click" });
+    }
+}
+
+async function getStatus() {
+    const response = await axios({
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("jwt"),
+        },
+        method: 'GET',
+        url: 'http://localhost:3000/account/status',
+    });
+    console.log(response);
 }
